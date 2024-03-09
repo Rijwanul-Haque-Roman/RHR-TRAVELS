8607d3334b0aeee92f9849fecafe0ad6790e9851
Rijwanul Haque Roman <rizwanul210058@diit.edu.bd>
2024-03-09 17:06:56 +0600

    fixed some UX realated  major issues and made the website more functional

--- JS/index.js
+++ JS/index.js
@@ -1,45 +1,93 @@
+function scrollToSection() {
+  const section = document.getElementById("ticket-purchase-section");
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
 function bgChange(elementId) {
   const targetElement = document.getElementById(elementId);
-  targetElement.classList.add("bg-[#1dd100]", "text-white");
-  targetElement.classList.remove("bg-[#F7F8F8]", "text-[#03071280]");
-}
-
-let seatList = [];
-function selectedSeats(elementId) {
-  if (seatList.includes(elementId) == false && seatList.length <= 3) {
-    seatList.push(document.getElementById(elementId).innerText);
-
-    bgChange(elementId);
-
-    document.getElementById("total-selected-seat").innerText = seatList.length;
-    document.getElementById("seat-left").innerText = 40 - seatList.length;
-    document.getElementById("total-price").innerText = seatList.length * 550;
-    document.getElementById("grand-total").innerText = seatList.length * 550;
-
-    let parent = document.getElementById("seat-selection");
-    let child = document.createElement("p");
-
-    child.innerText =
-      document.getElementById(elementId).innerText + " " + "AC_Business 550";
-    child.classList.add("word-spacing");
-    parent.appendChild(child);
+  if (targetElement.classList.contains("bg-[#F7F8F8]")) {
+    targetElement.classList.add("bg-[#1dd100]", "text-white");
+    targetElement.classList.remove("bg-[#F7F8F8]", "text-[#03071280]");
   } else {
-    window.alert("You can not select more than 4 tickets");
-  }
-
-  if (seatList.length === 4) {
-    document.getElementById("apply").removeAttribute("disabled");
-  }
-
-  if (seatList.length > 4) {
-    window.alert("You can not select more than 4 tickets");
+    targetElement.classList.remove("bg-[#1dd100]", "text-white");
+    targetElement.classList.add("bg-[#F7F8F8]", "text-[#03071280]");
   }
 }
+
+function ShowSelectedSeat(seatName) {
+  const parent = document.getElementById("seat-selection");
+
+  const container = document.createElement("div");
+  container.classList.add("flex", "justify-between");
+  container.id = seatName + "-seat";
+  console.log(container.id);
+
+  const seat = document.createElement("p");
+  const seatType = document.createElement("p");
+  const SeatPrice = document.createElement("p");
+
+  seat.innerText = seatName;
+  seatType.innerText = "AC_Business";
+  SeatPrice.innerText = "550";
+
+  container.appendChild(seat);
+  container.appendChild(seatType);
+  container.appendChild(SeatPrice);
+
+  parent.appendChild(container);
+}
+
+function deleteSeat(seatName) {
+  const seat = document.getElementById(seatName + "-seat");
+  console.log(seat);
+  seat.remove();
+}
+
+const seats = document.getElementById("allSeats");
+let selectedSeats = [];
+
+seats.addEventListener("click", function (event) {
+  const seat = event.target.innerText;
+
+  bgChange(seat);
+
+  if (!selectedSeats.includes(seat)) {
+    if (selectedSeats.length < 4) {
+      selectedSeats.push(seat);
+      ShowSelectedSeat(seat);
+    } else {
+      window.alert("You can not buy more than 4 seats");
+      bgChange(seat);
+    }
+  } else {
+    selectedSeats.splice(selectedSeats.indexOf(seat), 1);
+    deleteSeat(seat);
+  }
+
+  const seatLeft = document.getElementById("seat-left");
+  const totalSelectedSeats = document.getElementById("total-selected-seat");
+  seatLeft.innerText = 40 - selectedSeats.length;
+  totalSelectedSeats.innerText = selectedSeats.length;
+
+  document.getElementById("total-price").innerText = selectedSeats.length * 550;
+  document.getElementById("grand-total").innerText = selectedSeats.length * 550;
+
+  if (selectedSeats.length === 4) {
+    document.getElementById("apply").removeAttribute("disabled", true);
+  } else {
+    document.getElementById("apply").setAttribute("disabled", true);
+  }
+});
 
 function couponValidation() {
   const coupon = document.getElementById("coupon").value;
   if (coupon === "NEW15" || coupon === "Couple 20") {
     document.getElementById("gaibul-hawa").classList.add("hidden");
+
+    for (const seat of selectedSeats) {
+      document.getElementById(seat).setAttribute("disabled", true);
+    }
+
     document.getElementById("discount-container").classList.remove("hidden");
     if (coupon === "NEW15") {
       document.getElementById("discount").innerText = 330;
@@ -55,9 +103,17 @@ function couponValidation() {
 
 document.getElementById("phone").addEventListener("keyup", function (event) {
   const number = parseInt(event.target.value);
-  if (seatList.length !== 0 && typeof (number === "number") && !isNaN(number)) {
-    document.getElementById("next").removeAttribute("disabled");
+  if (
+    selectedSeats.length !== 0 &&
+    typeof (number === "number") &&
+    !isNaN(number)
+  ) {
+    document.getElementById("next").removeAttribute("disabled", true);
   } else {
     document.getElementById("next").setAttribute("disabled", true);
   }
 });
+
+function reload() {
+  window.location.reload();
+}
